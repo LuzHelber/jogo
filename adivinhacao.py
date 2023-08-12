6ac0e747e1949c08c2fbd0841bf706ea7e341a3a
Helber Luz <helbersap@gmail.com>
2023-08-12 15:30:31 -0300

    adicionado limitacao entre numero 1 e 50

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,9 +7,13 @@ total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas +1):
     print("Tentativa {} de {}".format( rodada, total_de_tentativas))
-    chute_str = input("Digite o seu numero: ")
+    chute_str = input("Digite um numero entre 1 e 50: ")
     print("Voce digitou ", chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 50):
+        print("Voce deve digitar um numero entre 1 e 50!")
+        continue
 
     acertou = numero_secreto == chute
     maior = chute > numero_secreto
